ef67e307ec24b2ae979d6a213455defead1ec285
Влад Чувашов <vlad.chuvasov@gmail.com>
2024-07-22 19:14:47 +0300

    Просьба!!!!!!!!! НЕ ЗАХОДИТЬ
    НЕ НУЖНО ЗАХОДИТЬ, ЭТО СЮРПРИЗ

---
+++ Main.js
@@ -0,0 +1,10 @@
+let password = prompt ("Введите пароль");
+
+if( password  == "02232010"){
+    alert("вход выполнен");
+    console.log("Подключился новый пользователь")
+}
+
+if(password !="02232010"){
+    alert("./surprise.html");
+}
